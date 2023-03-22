8691c90e9f6e79dca515a6c893e2addf9be9c5cf
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-22 13:16:35 +0300

    huge security improvements

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express()
 
@@ -21,8 +22,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -41,7 +40,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save().then(() => {
