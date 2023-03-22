b9de24157ad09fa53623af87c42aecc48835e833
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-22 11:48:31 +0300

    adding env vars

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
 
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express()
 
@@ -20,8 +22,9 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const User = new mongoose.model("User", userSchema)
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]})
 
+const User = new mongoose.model("User", userSchema)
 
 app.get("/", function (req, res) {
     res.render("home")
